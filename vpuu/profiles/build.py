f2ef334cc82e458a5013d86201d56f2c3adc9881
lunga <stumenz.complex@gmail.com>
2019-09-23 11:39:20 +0200

    add landcover stats

--- vpuu/profiles/build.py
+++ vpuu/profiles/build.py
@@ -1,6 +1,12 @@
 from dynamic_profile.utils import BuildIndicator
 from wazimap.data.tables import get_datatable
-from wazimap.data.utils import calculate_median
+from wazimap.data.utils import (
+    calculate_median,
+    dataset_context,
+    get_stat_data,
+    group_remainder,
+)
+from wazimap.models.data import DataNotFound
 
 
 class VpuuIndicator(BuildIndicator):
@@ -29,3 +35,31 @@ class VpuuIndicator(BuildIndicator):
         if self.profile.title == "Age":
             head["value"] = self.calculate_age_median()
         return head
+
+    def landcover(self):
+        with dataset_context(year="2014"):
+            try:
+                distribution, total = get_stat_data(
+                    [self.profile.field_name],
+                    self.geo,
+                    self.session,
+                    table_universe=self.profile.universe,
+                    table_dataset=self.profile.dataset,
+                    exclude_zero=self.profile.exclude_zero,
+                    percent=self.profile.percent,
+                    recode=self.profile.recode,
+                    key_order=self.profile.key_order,
+                    exclude=self.profile.exclude,
+                    order_by=self.profile.order_by,
+                )
+                group_remainder(distribution, self.profile.group_remainder)
+                self.distribution = distribution
+                return {"stat_values": distribution, "total": total}
+            except DataNotFound:
+                return {}
+
+    def calculation(self):
+        if self.profile.title == "National Land Cover":
+            return self.landcover()
+        else:
+            return super(VpuuIndicator, self).calculation()
