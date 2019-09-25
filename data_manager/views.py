6c53e921f33940828ef5a9b8ba7ddcedd2559a42
Delena Malan <delenamalan@gmail.com>
2019-09-25 16:20:46 +0200

    Add admin decorator

--- data_manager/views.py
+++ data_manager/views.py
@@ -8,6 +8,7 @@ from django.template import loader
 from django import forms
 from django.contrib.admin import widgets
 from django.contrib.messages import api as messages_api
+from django.contrib.admin.views.decorators import staff_member_required
 
 from wazimap.models import FieldTable
 from dataset_upload import UploadedDataSet
@@ -21,8 +22,8 @@ class DataUploadForm(forms.Form):
     data_file = forms.FileField(widget=widgets.AdminFileWidget)
 
 
+@staff_member_required
 def add_dataset(request):
-    # TODO: secure route
     if request.method == 'POST':
         form = DataUploadForm(request.POST, request.FILES)
         if form.is_valid():
@@ -34,7 +35,8 @@ def add_dataset(request):
                 return HttpResponseRedirect('/admin/')
             except Exception as e:
                 log.debug(e)
-                messages_api.error(request, "Error occurred while uploading dataset.")
+                messages_api.error(request,
+                                   "Error occurred while uploading dataset.")
     else:
         form = DataUploadForm()
     return render(request, 'data_manager/dataset_form.html', {
