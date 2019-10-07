9c6ec9e183ab7560953536013a84006769872ddb
lunga <stumenz.complex@gmail.com>
2019-10-07 11:48:21 +0200

    format for python3

--- data_manager/views.py
+++ data_manager/views.py
@@ -11,7 +11,7 @@ from django.contrib.messages import api as messages_api
 from django.contrib.admin.views.decorators import staff_member_required
 
 from wazimap.models import FieldTable
-from dataset_upload import UploadedDataSet
+from .dataset_upload import UploadedDataSet
 
 log = logging.getLogger(__name__)
 
@@ -24,26 +24,28 @@ class DataUploadForm(forms.Form):
 
 @staff_member_required
 def add_dataset(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = DataUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            data_file = request.FILES['data_file']
+            data_file = request.FILES["data_file"]
             try:
-                handle_uploaded_dataset(request.FILES['data_file'],
-                                        form.cleaned_data['field_table'])
+                handle_uploaded_dataset(
+                    request.FILES["data_file"], form.cleaned_data["field_table"]
+                )
                 messages_api.success(request, "Successfully uploaded dataset!")
-                return HttpResponseRedirect('/admin/')
+                return HttpResponseRedirect("/admin/")
             except Exception as e:
                 log.debug(e)
                 messages_api.error(
-                    request,
-                    "Error occurred while uploading dataset: {}".format(e))
+                    request, "Error occurred while uploading dataset: {}".format(e)
+                )
     else:
         form = DataUploadForm()
-    return render(request, 'data_manager/dataset_form.html', {
-        'model_name': 'Field table',
-        'form': form,
-    })
+    return render(
+        request,
+        "data_manager/dataset_form.html",
+        {"model_name": "Field table", "form": form},
+    )
 
 
 def handle_uploaded_dataset(f, field_table):
