81730e4c7f7c6cc982832d80e46fb60e2ec02f23
lunga <stumenz.complex@gmail.com>
2019-11-26 12:03:52 +0200

    add config options

--- vpuu/settings.py
+++ vpuu/settings.py
@@ -138,5 +138,6 @@ TEMPLATES = [
 WKHTMLTOPDF_CMD_OPTIONS = {
     "print-media-type": True,
     "debug-javascript": True,
+    "viewport-size": "1920x1080",
     "run-script": "javascript:($(function(){window.browserWidth=1920;window.browserHeight=1080;makeCharts();cloneCharts()}))",
 }
