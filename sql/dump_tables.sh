8031b52a5a73080dbade0ef07c95d7f7ac0e0603
lunga <stumenz.complex@gmail.com>
2019-12-02 14:05:00 +0200

    update fixtures

--- sql/dump_tables.sh
+++ sql/dump_tables.sh
@@ -4,6 +4,7 @@ export PGPASSWORD=wazimap_vpuu
 
 for t in `ls census/[a-z]*.sql`
 do
+    echo "Working on $t";
     pg_dump "postgres://wazimap_vpuu@development/wazimap_vpuu" \
         -O -c --if-exists -t $(basename $t .sql) \
       | egrep -v "(idle_in_transaction_session_timeout|row_security)" \
