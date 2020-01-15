f329dab99be870020755f9a5b2de00824a7adbb3
lunga <stumenz.complex@gmail.com>
2020-01-15 16:51:44 +0200

    remove query parameter if not provided

--- vpuu/profiles/views.py
+++ vpuu/profiles/views.py
@@ -13,13 +13,11 @@ log = logging.getLogger(__name__)
 class ProfilePDFView(GeographyDetailView):
     def get(self, request, *args, **kwargs):
         profiles = request.GET.get("profiles")
-        url = f"/profiles/{self.geo_level}-{self.geo_code}-{self.geo.slug}/?profiles={profiles}"
-        # url = "/profiles/%s-%s-%s/?print=1" % (
-        #     self.geo_level,
-        #     self.geo_code,
-        #     self.geo.slug,
-        # )
-        log.debug(url)
+        if profiles:
+            url = f"/profiles/{self.geo_level}-{self.geo_code}-{self.geo.slug}/?profiles={profiles}"
+        else:
+            url = f"/profiles/{self.geo_level}-{self.geo_code}-{self.geo.slug}"
+
         url = request.build_absolute_uri(url)
         pdf = wkhtmltopdf(url)
         filename = "%s-%s-%s.pdf" % (self.geo_level, self.geo_code, self.geo.slug)
